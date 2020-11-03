98346b4a7f82ba622a0021476aadef650d57cd4b
almeidx <almeidx@pm.me>
2020-11-03 22:48:36 +0000

    first commit

---
+++ FlappyBird/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MainWindow = New System.Windows.Forms.PictureBox()
+        Me.Render = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.MainWindow, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MainWindow
+        '
+        Me.MainWindow.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MainWindow.Location = New System.Drawing.Point(0, 0)
+        Me.MainWindow.Name = "MainWindow"
+        Me.MainWindow.Size = New System.Drawing.Size(1000, 1000)
+        Me.MainWindow.TabIndex = 0
+        Me.MainWindow.TabStop = False
+        '
+        'Render
+        '
+        Me.Render.Enabled = True
+        Me.Render.Interval = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1000, 1000)
+        Me.Controls.Add(Me.MainWindow)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.MainWindow, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MainWindow As PictureBox
+    Friend WithEvents Render As Timer
+End Class
---
+++ FlappyBird/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+    Public Shared BMP As Bitmap
+
+    Public Shared GRX As Graphics
+
+    Public Shared _bird
+
+    Public Shared pipeList = New List(Of Pipe)()
+
+    Public Shared _pipe
+
+    Public Shared counter As Double
+
+    Public Structure Bird
+        Public h
+        Public vel
+
+        Public Sub Update(force As Double, elapsedTime As Double)
+            vel += force * elapsedTime
+            h += vel
+        End Sub
+    End Structure
+
+    Public Class Pipe
+        Public x
+
+        Public gap
+
+        Public yOff
+    End Class
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BMP = New Bitmap(MainWindow.Width, MainWindow.Height)
+        GRX = Graphics.FromImage(BMP)
+
+        ' mid = GetRandom((MainWindow.Height / 2) - 200, (MainWindow.Height / 2) + 200)
+        ' gap = GetRandom(100, 200)
+
+        _bird = New Bird() With {.h = 700, .vel = 0}
+
+        pipeList.Add(New Pipe() With {.x = MainWindow.Width, .gap = 200, .yOff = 50})
+    End Sub
+
+    Private Sub Render_Tick(sender As Object, e As EventArgs) Handles Render.Tick
+        Clear(0, 0, 0, 255)
+
+        Dim change = Render.Interval
+
+        _bird.Update(-20, Render.Interval / 1000)
+
+        Text = $"Flappy Bird - FPS: {1000 / Render.Interval}"
+
+        pipeList(0).x -= 5
+
+        Dim rectTop = New Rectangle(pipeList(0).x, (MainWindow.Height / 2) - (pipeList(0).gap / 2) - 800 + pipeList(0).yOff, 75, 800)
+        Dim rectBottom = New Rectangle(pipeList(0).x, (MainWindow.Height / 2) + (pipeList(0).gap / 2) + pipeList(0).yOff, 75, 800)
+        GRX.FillRectangle(Brushes.DarkGreen, rectTop)
+        GRX.FillRectangle(Brushes.DarkGreen, rectBottom)
+
+        GRX.FillEllipse(Brushes.Red, CInt(MainWindow.Width / 2) - 25, CInt(MainWindow.Height - _bird.h), 25, 25)
+
+
+        ' GRX.DrawString((MainWindow.Height - (mid - gap / 2)).ToString(), New Font("Arial", 20), Brushes.White, 0, 0)
+        ' GRX.DrawString(mid.ToString(), New Font("Arial", 20), Brushes.White, 0, 30)
+        ' GRX.DrawString(gap.ToString(), New Font("Arial", 20), Brushes.White, 0, 60)
+
+        MainWindow.Image = BMP
+    End Sub
+
+    Private Sub Clear(r, g, b, a)
+        Dim brush = New SolidBrush(Color.FromArgb(a, r, g, b))
+        GRX.FillRectangle(brush, 0, 0, MainWindow.Width, MainWindow.Height)
+    End Sub
+
+    Public Function GetRandom(Min As Integer, Max As Integer) As Integer
+        Static Generator As Random = New Random()
+        Return Generator.Next(Min, Max)
+    End Function
+
+    Public Sub MyBase_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+        If e.KeyChar = ChrW(Keys.Space) Then
+            _bird.vel = 5
+        End If
+    End Sub
+End Class
